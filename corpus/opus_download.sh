628c7ad11acae4eab38aba7f4959d7c5bce44ba1
Constantin Nicolae <dragosnicolae555@gmail.com>
2021-07-17 09:10:10 +0200

    update opus

--- corpus/opus_download.sh
+++ corpus/opus_download.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Downloading OPUS... "
+python3 -m pip install opustools-pkg
 echo "  * If you see an error running opus_get, please run 'pip install -r requirements.txt"
 mkdir -p raw/opus
 cd raw/opus
 opus_get -s ro -p raw -q
 unzip -o -q \*.zip
+gunzip *.gz
 rm *.zip
+rm *.gz
 rm INFO
 rm LICENSE
 rm README
